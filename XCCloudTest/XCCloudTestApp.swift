0aa3a1f14310d1917d709f2d6bedc40fe5c11096
James Goodwill <james.goodwill@smartrent.com>
2023-02-21 15:39:03 -0800

    Initial Commit

---
+++ XCCloudTest/XCCloudTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCCloudTestApp.swift
+//  XCCloudTest
+//
+//  Created by James Goodwill on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct XCCloudTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
